ea6ba3c4139197998b3719755fd53e1033a1d463
manailma <max.manaila@eawag.ch>
2025-07-08 13:14:53 +0200

    adapted driver

--- src/drivers/simstrat/fabm_driver.h
+++ src/drivers/simstrat/fabm_driver.h
@@ -1,9 +1,12 @@
+! model dimension
 #define _FABM_DIMENSION_COUNT_ 1
 #define _FABM_DEPTH_DIMENSION_INDEX_ 1
 #define _FABM_VECTORIZED_DIMENSION_INDEX_ 1
 
+! vertical index is ordered from bottom to surface
 #define _FABM_VERTICAL_BOTTOM_TO_SURFACE_
 
-#define _FABM_CONTIGUOUS_
+! if all arrays passed to FABM are contiguous in memory
+! #define _FABM_CONTIGUOUS_
 
 #include "fabm.h"

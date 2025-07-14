38755b2a599f5b3d885f458a7b0a2387a4865e52
manailma <max.manaila@eawag.ch>
2025-07-14 17:43:40 +0200

    adapted driver for bottom_everywhere

--- src/drivers/simstrat/fabm_driver.h
+++ src/drivers/simstrat/fabm_driver.h
@@ -6,6 +6,9 @@
 ! vertical index is ordered from bottom to surface
 #define _FABM_VERTICAL_BOTTOM_TO_SURFACE_
 
+! vertical index can vary (required for bottom_everywhere)
+#define _FABM_BOTTOM_INDEX_ -1
+
 ! pass if all arrays passed to FABM are contiguous in memory
 ! #define _FABM_CONTIGUOUS_
 
